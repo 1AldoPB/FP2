40179876a4d1a2f37064386cd1f9480b560ca89f
1AldoPB <L25550357@chihuahua2.tecnm.mx>
2025-10-05 21:54:45 -0600

    Add files via upload

---
+++ EVA2_8_FOR/src/eva2_8_for/EVA2_8_FOR.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_8_for;
+
+/**
+ *
+ * @author aldop
+ */
+public class EVA2_8_FOR {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //3 parametors(variable y valor),(hasta donde llega(es una condicion tiene que ser verdadera)),(incremento
+        //cuando es falso el for se detiene
+        //SUMAR O RESTAR:
+        //AÑADIR 1 O RESTAR 1
+        //++ O --
+        //i++ significa sumale 1 al valor de la i
+        for(int i=1;i <=10;i++){
+        System.out.println("Hola mundo");
+        System.out.println(i);
+        }
+        //i-- significa restarle 1 al valor de i
+         for(int i=10;i >=1;i--){
+        System.out.println("Hola mundo");
+        System.out.println(i);
+        }
+
+    }
+    
+}
