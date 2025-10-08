1f8d51997ab959d8e3a835a98439061d460ecd6b
1AldoPB <L25550357@chihuahua2.tecnm.mx>
2025-10-08 16:47:48 -0600

    Add files via upload

---
+++ EVA2_14_CAPTURA/src/eva2_14_captura/EVA2_14_CAPTURA.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_14_captura;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author aldop
+ */
+public class EVA2_14_CAPTURA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int tama;
+        System.out.println("Cuantas calificaciones necesitas");
+        tama=sc.nextInt();
+        int califas[]=new int[tama];
+        //capturar
+        for(int i=0;i<califas.length;i++){
+            System.out.println("calificacion"+ (i +1) + ":");
+        califas[i]=sc.nextInt();
+        //leer
+        }for(int i=0;i<califas.length;i++){
+            System.out.println("[" + califas[i]+"]");
+         
+        }
+    }
+    
+}
